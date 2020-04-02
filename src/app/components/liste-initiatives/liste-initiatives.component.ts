54da69880967bf14a5bd899589cb5acddb09fb77
Clement <clement.le.bars@accenture.com>
2020-04-02 21:12:50 +0200

    Add cards in grid

--- src/app/components/liste-initiatives/liste-initiatives.component.ts
+++ src/app/components/liste-initiatives/liste-initiatives.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListeInitiativesComponent implements OnInit {
 
+  liste = [
+    {name: 'Diego' , age: 30},
+    {name: 'Diego' , age: 27},
+    {name: 'Diego' , age: 31},
+    {name: 'Diego' , age: 17},
+    {name: 'Diego' , age: 23},
+    {name: 'Diego' , age: 30},
+    {name: 'Diego' , age: 26},
+    {name: 'Diego' , age: 30},
+    {name: 'Diego' , age: 67},
+    {name: 'Diego' , age: 30},
+    {name: 'Diego' , age: 34},
+    {name: 'Diego' , age: 28}
+  ];
   constructor() { }
 
   ngOnInit(): void {
