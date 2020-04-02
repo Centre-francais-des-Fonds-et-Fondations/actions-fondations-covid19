a1e42bbc442d5b53e7a9b6448f1efbffd9790090
Fraval <ludovic.fraval@accenture.com>
2020-04-02 17:18:56 +0200

    fix(components): delete formulaires & iframe)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { FormulaireBesoinSoutienComponent } from './components/formulaire-besoin-soutien/formulaire-besoin-soutien.component';
-import { FormulaireContactComponent } from './components/formulaire-contact/formulaire-contact.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 
 
 const routes: Routes = [
-  { path: 'formulaireBesoinSoutien', component: FormulaireBesoinSoutienComponent },
-  { path: 'formulaireContact', component: FormulaireContactComponent },
   { path: '', component: HomepageComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './share/nav/nav.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { FormulaireBesoinSoutienComponent } from './components/formulaire-besoin-soutien/formulaire-besoin-soutien.component';
-import { FormulaireContactComponent } from './components/formulaire-contact/formulaire-contact.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FormulaireBesoinSoutienComponent,
-    FormulaireContactComponent,
     HomepageComponent
   ],
   imports: [
