665395bd77f2cbac31dae366709c9442e51286ab
Clement <clement.le.bars@accenture.com>
2020-04-03 11:41:48 +0200

    link airtable data with vignette + modify vignette

--- src/app/components/liste-initiatives/liste-initiatives.component.ts
+++ src/app/components/liste-initiatives/liste-initiatives.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Airtable from '../../../airtable';
 
 @Component({
   selector: 'app-liste-initiatives',
@@ -7,50 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListeInitiativesComponent implements OnInit {
 
-  listeInitiatives = [
-    {
-      type: 'Soutien',
-      nomStructure: '1% for the planet France',
-      descriptionAction: 'Notre fondation oeuvre essentiellement dans les TESTS',
-      descriptionInitiative: `Carte de présentation d'une proposition de soutien`,
-      descriptionSoutienNature: `Cette demande ne s'accompagne pas de soutien offert en nature`,
-      montant: '1500€',
-      lienPage: 'https://www.google.fr',
-      urlImg: 'https://www.centre-francais-fondations.org/logo.png'
-    },
-    {
-      type: 'Besoin',
-      nomStructure: 'Pourquoi pas',
-      descriptionAction: 'Notre fondation oeuvre essentiellement dans les TESTS Notre fondation oeuvre essentiellement dans les TESTS Notre fondation oeuvre essentiellement dans les TESTS',
-      descriptionInitiative: `Carte de présentation d'une proposition de soutien`,
-      descriptionSoutienNature: `Cette demande ne s'accompagne pas de soutien offert en nature`,
-      lienPage: 'https://www.google.fr',
-      urlImg: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/LOGO_Fondation_du_patrimoine.jpg'
-    },
-    {
-      type: 'Soutien',
-      nomStructure: 'Banque de France',
-      descriptionAction: 'Notre fondation oeuvre essentiellement dans les TESTS',
-      descriptionInitiative: `Carte de présentation d'une proposition de soutien`,
-      descriptionSoutienNature: `Cette demande ne s'accompagne pas de soutien offert en nature`,
-      montant: '27000€',
-      lienPage: 'https://www.google.fr',
-      urlImg: 'https://www.captifs.fr/wp-content/uploads/2019/04/Logo-Fondation-Orange-953x536.jpg'
-    }
-  ];
-
-  nomStructure = '1% for the planet France';
-  typeInitiative = 'Soutien';
-  descriptionAction = 'Notre fondation oeuvre essentiellement dans les TESTS';
-  descriptionInitiative = `Carte de présentation d'une proposition de soutien`;
-  descriptionSoutienNature = `Cette demande ne s'accompagne pas de soutien offert en nature`;
-  montant = '1500000€';
-  lienPage = 'https://www.google.fr';
-  urlImg = 'https://www.centre-francais-fondations.org/logo.png';
+  listeInitiatives = {};
 
   constructor() { }
 
   ngOnInit(): void {
+    Airtable.getInitiatives()
+    .then((AAA) => {
+      console.log('AAA');
+      console.log(AAA);
+      this.listeInitiatives = AAA;
+    }).catch();
   }
 
 }
--- src/app/components/vignette/vignette.component.ts
+++ src/app/components/vignette/vignette.component.ts
@@ -9,15 +9,6 @@ export class VignetteComponent implements OnInit {
 
   @Input() initiative: any;
 
-  nomStructure = '1% for the planet France';
-  typeInitiative = 'Soutien';
-  descriptionAction = 'Notre fondation oeuvre essentiellement dans les TESTS';
-  descriptionInitiative = `Carte de présentation d'une proposition de soutien`;
-  descriptionSoutienNature = `Cette demande ne s'accompagne pas de soutien offert en nature`;
-  montant = '1500€';
-  lienPage = 'https://www.google.fr';
-  urlImg = 'https://www.centre-francais-fondations.org/logo.png';
-
   constructor() { }
 
   ngOnInit(): void {
