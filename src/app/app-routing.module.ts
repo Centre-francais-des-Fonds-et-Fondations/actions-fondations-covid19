118b4bd782f181b20091dd6372120d680da14315
Fraval <ludovic.fraval@accenture.com>
2020-04-06 19:10:38 +0200

    feature(cta): add cta page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { CtaComponent } from './components/cta/cta.component';
 
 
 const routes: Routes = [
-  { path: '', component: HomepageComponent }
+  { path: '', component: HomepageComponent },
+  { path: 'cta', component: CtaComponent }
 ];
 
 @NgModule({
