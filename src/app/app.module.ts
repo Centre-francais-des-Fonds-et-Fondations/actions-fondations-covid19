bc24deca2048efe1243f191d74ad087db6bd1cfd
Hospice <johannhospice.dev@gmail.com>
2020-04-09 10:19:42 +0200

    Adding local fr for currency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+
+import { faStackOverflow, faGithub, faMedium, faFacebook, faTwitter, faLinkedin, faYoutube, faFirefox, faEdge, faChrome } from '@fortawesome/free-brands-svg-icons';
+import { faSquare, faCheckSquare } from '@fortawesome/free-solid-svg-icons';
+import { faSquare as farSquare, faCheckSquare as farCheckSquare } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { VignetteComponent } from './components/vignette/vignette.component';
-import { NavComponent } from './share/nav/nav.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { ListeInitiativesComponent } from './components/liste-initiatives/liste-initiatives.component';
-
-// import { MatButtonModule } from '@angular/material/button';
-// import { MatCardModule } from '@angular/material/card';
-// import { MatDividerModule } from '@angular/material/divider';
-// import { MatGridListModule } from '@angular/material/grid-list';
-// import { MatToolbarModule } from '@angular/material/toolbar';
 import { JumbotronComponent } from './components/jumbotron/jumbotron.component';
+import { CtaComponent } from './components/cta/cta.component';
+import { NavComponent } from './share/nav/nav.component';
 import { FooterComponent } from './share/footer/footer.component';
 import { FooterWrapperComponent } from './share/footer/footer-wrapper/footer-wrapper.component';
-import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { faSquare, faCheckSquare } from '@fortawesome/free-solid-svg-icons';
-import { faSquare as farSquare, faCheckSquare as farCheckSquare } from '@fortawesome/free-regular-svg-icons';
-import { faStackOverflow, faGithub, faMedium, faFacebook, faTwitter, faLinkedin, faYoutube, faFirefox, faEdge, faChrome } from '@fortawesome/free-brands-svg-icons';
-import { CtaComponent } from './components/cta/cta.component';
 
 
 @NgModule({
@@ -47,8 +44,9 @@ import { CtaComponent } from './components/cta/cta.component';
 })
 export class AppModule {
   constructor(private library: FaIconLibrary) {
+    registerLocaleData(localeFr);
     library.addIcons(faSquare, faCheckSquare, farSquare
-        , farCheckSquare, faStackOverflow, faGithub, faMedium,
-        faFacebook, faTwitter, faLinkedin, faYoutube, faFirefox, faEdge, faChrome);
+      , farCheckSquare, faStackOverflow, faGithub, faMedium,
+      faFacebook, faTwitter, faLinkedin, faYoutube, faFirefox, faEdge, faChrome);
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  airtable: {
+    apiKey: 'keyTs9N5SsXPWc03P',
+    baseId: 'appyiU5U8JBAf23m2',
+    tableName: 'Initiatives',
+    viewName: 'Liste des initiatives publiées'
+  }
 };
