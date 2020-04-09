8a6a0ae69ccf4663a16f62f6173713acf00e18fb
Hospice <johannhospice.dev@gmail.com>
2020-04-09 12:42:31 +0200

    Adding filters on areas and zones

--- src/app/components/liste-initiatives/liste-initiatives.component.ts
+++ src/app/components/liste-initiatives/liste-initiatives.component.ts
@@ -10,7 +10,27 @@ export class ListeInitiativesComponent implements OnInit {
   pageReader: any;
   query = '';
   queryType = '';
+  queryZone = '';
+  queryArea = '';
   PAGE_SIZE = 12;
+
+  types = ['Besoin', 'Soutien'];
+  areas = [
+    "Recherche",
+    "Education",
+    "Hôpitaux",
+    "Emploi",
+    "Action internationale",
+    "Solidarité",
+    "Médico-social",
+    "Autre",
+    "Santé"
+  ];
+  zones = [
+    "Internationale",
+    "Nationale",
+    "Régionale"
+  ];
 
   callState = 'await';
   async ngOnInit() {
@@ -31,22 +51,38 @@ export class ListeInitiativesComponent implements OnInit {
     this.queryType = event.target.value.toLowerCase();
     this.search();
   }
+  onSelectArea(event: any) {
+    this.queryArea = event.target.value.toLowerCase();
+    this.search();
+  }
+  onSelectZone(event: any) {
+    this.queryZone = event.target.value.toLowerCase();
+    this.search();
+  }
 
   search() {
+    const match = (from, q) => {
+      if (q.length > 0) {
+        return from.toLowerCase().match(new RegExp(`${q}.*`));
+      }
+      return true;
+    }
     this.pageReader.setIndex(0);
     this.pageReader.search(
       (record) => {
         // si rien, tout afficher
-        if (this.query.length === 0 && this.queryType.length === 0) {
+        if (this.query.length === 0 && this.queryType.length === 0 && this.queryArea.length === 0 && this.queryZone.length === 0) {
           return true;
         }
         // sinon ...
         let valid = true;
-        if (this.query.length > 0) {
-          valid = record.structureName.toLowerCase().match(new RegExp(`${this.query}.*`));
+        valid = valid && match(record.structureName, this.query);
+        valid = valid && match(record.initiativeType, this.queryType);
+        if (this.queryArea.length > 0) {
+          valid = valid && record.interventionAreas && record.interventionAreas.some(field => field.toLowerCase() === this.queryArea);
         }
-        if (this.queryType.length > 0) {
-          valid = valid && record.initiativeType.toLowerCase().match(new RegExp(`${this.queryType}.*`));
+        if (this.queryZone.length > 0) {
+          valid = valid && record.interventionZone && record.interventionZone.some(field => field.toLowerCase() === this.queryZone);
         }
         return valid;
       }
