293dad6b05d4c9d84ca782d7f81f6f5510a28441
Hospice <johannhospice.dev@gmail.com>
2020-04-10 00:41:28 +0200

    Adding "Région" field and update style add function to scroll to an element

--- src/app/components/liste-initiatives/liste-initiatives.component.ts
+++ src/app/components/liste-initiatives/liste-initiatives.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { getInitiatives, getInitiativesByPages, PageReader } from '../../../airtable';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-liste-initiatives',
@@ -7,32 +8,54 @@ import { getInitiatives, getInitiativesByPages, PageReader } from '../../../airt
   styleUrls: ['./liste-initiatives.component.scss']
 })
 export class ListeInitiativesComponent implements OnInit {
+  PAGE_SIZE = 21;
+  callState = 'await';
   pageReader: any;
+
   query = '';
   queryType = '';
   queryZone = '';
   queryArea = '';
-  PAGE_SIZE = 12;
+  queryRegion = '';
 
   types = ['Besoin', 'Soutien'];
   areas = [
-    "Recherche",
-    "Education",
-    "Hôpitaux",
-    "Emploi",
-    "Action internationale",
-    "Solidarité",
-    "Médico-social",
-    "Autre",
-    "Santé"
+    'Recherche',
+    'Education',
+    'Hôpitaux',
+    'Emploi',
+    'Action internationale',
+    'Solidarité',
+    'Médico-social',
+    'Santé',
+    'Autre'
   ];
   zones = [
-    "Internationale",
-    "Nationale",
-    "Régionale"
+    'Internationale',
+    'Nationale',
+    'Régionale'
+  ];
+  regions = [
+    'Grand Est',
+    'Nouvelle-Aquitaine',
+    'Auvergne-Rhône-Alpes',
+    'Bourgogne-Franche-Comté',
+    'Bretagne',
+    'Centre-Val de Loire',
+    'Corse',
+    'Hauts-de-France',
+    'Île-de-France',
+    'Occitanie',
+    'Normandie',
+    'Pays de la Loire',
+    'Provence-Alpes-Côte d\'Azur',
+    'Guadeloupe',
+    'Guyane',
+    'Martinique',
+    'Réunion',
+    'Mayotte'
   ];
 
-  callState = 'await';
   async ngOnInit() {
     try {
       const data = await getInitiativesByPages(this.PAGE_SIZE);
@@ -59,21 +82,29 @@ export class ListeInitiativesComponent implements OnInit {
     this.queryZone = event.target.value.toLowerCase();
     this.search();
   }
-
+  onSelectRegion(event: any) {
+    this.queryRegion = event.target.value.toLowerCase();
+    this.search();
+  }
   search() {
-    const match = (from, q) => {
+    const match = (from: string, q: string): any => {
       if (q.length > 0) {
         return from.toLowerCase().match(new RegExp(`${q}.*`));
       }
       return true;
-    }
+    };
     this.pageReader.setIndex(0);
     this.pageReader.search(
       (record) => {
         // si rien, tout afficher
-        if (this.query.length === 0 && this.queryType.length === 0 && this.queryArea.length === 0 && this.queryZone.length === 0) {
+        if (this.query.length === 0
+          && this.queryType.length === 0
+          && this.queryArea.length === 0
+          && this.queryZone.length === 0
+          && this.queryRegion.length === 0) {
           return true;
         }
+        console.log(record.region);
         // sinon ...
         let valid = true;
         valid = valid && match(record.structureName, this.query);
@@ -84,8 +115,17 @@ export class ListeInitiativesComponent implements OnInit {
         if (this.queryZone.length > 0) {
           valid = valid && record.interventionZone && record.interventionZone.some(field => field.toLowerCase() === this.queryZone);
         }
+        if (this.queryRegion.length > 0) {
+          valid = valid && record.region && record.region.some(field => field.toLowerCase() === this.queryRegion);
+        }
         return valid;
       }
     );
+  }
+  gotoSearch(element: HTMLElement) {
+    window.scrollTo({
+      top: element.offsetTop - 54,
+      behavior: 'smooth'
+    });
   }
 }
